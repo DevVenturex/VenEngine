c7b213fe36521c5291758ae6be562d253237b65d
Sebastian Fromm <dev.venturex@gmail.com>
2025-02-11 23:13:47 +0100

    Create Render Pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use log::error;
+use winit::event_loop::{ControlFlow, EventLoop};
+use crate::application::Application;
+
+mod application;
+
 fn main() {
-    println!("Hello, world!");
+    unsafe { env::set_var("RUST_LOG", "debug") };
+    env_logger::init();
+
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = Application::default();
+    if let Err(e) = event_loop.run_app(&mut app) {
+        error!("Event loop error: {}", e);
+    }
 }
+
